7fd8445a9fd3ebce3539d2e79282c4508a88f91c
Gregor Petrin <gregap@gmail.com>
2019-02-12 19:24:09 +0100

    Replace mock hello world handler with a generic static file handler

--- repl/src/main/java/co/petrin/WebInterface.java
+++ repl/src/main/java/co/petrin/WebInterface.java
@@ -1,13 +1,20 @@
 package co.petrin;
 
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class WebInterface {
 
     public static void main(String[] args) {
-        Vertx.vertx()
+        Vertx vertx = Vertx.vertx();
+
+        Router router = Router.router(vertx);
+        router.route("/*").handler(StaticHandler.create());
+
+        vertx
         .createHttpServer()
-        .requestHandler(req -> req.response().end("Hello World!"))
+        .requestHandler(router)
         .listen(8080, handler -> {
             if (handler.succeeded()) {
                 System.out.println("http://localhost:8080/");

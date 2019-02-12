ed48e2dd1b12569198d9684cf591ba35ecc60b1b
Gregor Petrin <gregap@gmail.com>
2019-02-12 06:07:27 +0100

    Create a Vertx & Java hello world with Gradle

---
+++ repl/src/main/java/co/petrin/WebInterface.java
@@ -0,0 +1,20 @@
+package co.petrin;
+
+import io.vertx.core.Vertx;
+
+public class WebInterface {
+
+    public static void main(String[] args) {
+        Vertx.vertx()
+        .createHttpServer()
+        .requestHandler(req -> req.response().end("Hello World!"))
+        .listen(8080, handler -> {
+            if (handler.succeeded()) {
+                System.out.println("http://localhost:8080/");
+            } else {
+                System.err.println("Failed to listen on port 8080");
+            }
+        });
+    }
+
+}

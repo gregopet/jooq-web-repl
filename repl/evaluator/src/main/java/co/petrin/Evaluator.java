6ef0d4c595e59fb6ad174a1944eb100ff591b805
Gregor Petrin <gregap@gmail.com>
2019-02-13 17:55:44 +0100

    Add a stub script evaluator
    Put it into its own project so we can instantiate it separately

---
+++ repl/evaluator/src/main/java/co/petrin/Evaluator.java
@@ -0,0 +1,9 @@
+package co.petrin;
+
+public class Evaluator {
+
+    public String evaluate(String scriotToEvaluate) {
+        return "You wanted me to evaluate " + scriotToEvaluate;
+    }
+    
+}

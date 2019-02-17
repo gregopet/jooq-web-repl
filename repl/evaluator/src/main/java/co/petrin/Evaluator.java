72ebf92cefe2d1587e4fe15dc567d4d4b1b72761
Gregor Petrin <gregap@gmail.com>
2019-02-17 08:17:04 +0100

    Try to describe evaluation errors more gracefully

--- repl/evaluator/src/main/java/co/petrin/Evaluator.java
+++ repl/evaluator/src/main/java/co/petrin/Evaluator.java
@@ -2,8 +2,10 @@ package co.petrin;
 
 import jdk.jshell.*;
 import jdk.jshell.Snippet.Status;
+import org.jooq.tools.StringUtils;
 
 import java.util.List;
+import java.util.Locale;
 
 public class Evaluator {
 
@@ -20,19 +22,47 @@ public class Evaluator {
             }
 
             var event = events.get(0);
-            switch(event.status()) {
+            switch (event.status()) {
                 case VALID:
-                    return event.value();
+                    return StringUtils.defaultString(event.value(), "");
                 case REJECTED:
                     if (event.exception() != null) {
                         return event.exception().getMessage();
                     } else {
-                        return event.snippet().toString();
+                        return js.diagnostics(event.snippet())
+                            .map( d -> getErrorMessage(event, d))
+                            .collect(Collectors.joining("\n"));
                     }
                 default:
                     throw new IllegalStateException("This state was not programmed for, blame the programmer!");
             }
         }
+    private static String getErrorMessage(SnippetEvent event, Diag diag) {
+        String snippetText = event.snippet().source();
+        int position = (int)diag.getPosition(); // long-sizes snippets? Nah...
+        String textBeforeError = snippetText.substring(0, position);
+        int newlinesBefore = newlinesInString(textBeforeError);
+        int charactersAfterError = position - textBeforeError.length();
+        return diag.getMessage(Locale.US) + " (row " + (newlinesBefore + 1) + ", character " + charactersAfterError + ")";
+
     }
-    
+
+    private static int newlinesInString(String str) {
+        char[] chars = str.toCharArray();
+        int newlines = 0;
+        for (int pos = 0; pos < chars.length; pos++) {
+            if (isNewline(chars[pos])) {
+                newlines++;
+                if (pos + 1 < chars.length && isNewline(chars[pos + 1]) && chars[pos] != chars[pos+1]) {
+                    // ignore double-newline systems
+                    pos++;
+                }
+            }
+        }
+        return newlines;
+    }
+
+    private static boolean isNewline(char c) {
+        return c == '\n' || c == '\r';
+    }
 }

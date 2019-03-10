aaa5accae83a552cb0f9154b8552674a061f2912
Gregor Petrin <gregap@gmail.com>
2019-03-10 21:13:54 +0100

    Add more unit tests regarding exception reporting

--- repl/evaluator/src/test/groovy/OutputSpec.groovy
+++ repl/evaluator/src/test/groovy/OutputSpec.groovy
@@ -59,8 +59,9 @@ class OutputSpec extends Specification {
         result.error
 
         where:
-        input               | expectedOutput
-        '"asd'              | 'unclosed string literal (row 1, character 0)\nreached end of file while parsing (row 1, character 0)'
+        input          | expectedOutput
+        '"asd'         | 'unclosed string literal (row 1, character 0)\nreached end of file while parsing (row 1, character 0)'
+        '123;\n"asd'   | 'unclosed string literal (row 2, character 0)\nmissing return statement (row 2, character 0)'
     }
 
     def "Runtime exceptions are reported back with line & column information"() {
@@ -74,5 +75,18 @@ class OutputSpec extends Specification {
         where:
         input               | expectedOutput
         '1 / 0'             | 'java.lang.ArithmeticException: / by zero'
+    }
+
+    def "Exceptions are reported with correct line & column information even when preceeded by imports"() {
+        given: 'a script with leading imports'
+        def script = """\
+        import java.io.File;
+        var x = 5;
+        import java.util.Collection;
+        "ab
+        """.stripIndent()
+
+        expect: 'error to be reported in the correct place'
+        evaluator.evaluate(null, new EvaluationRequest(script, 0)).output == 'unclosed string literal (row 4, character 0)'
     }
 }

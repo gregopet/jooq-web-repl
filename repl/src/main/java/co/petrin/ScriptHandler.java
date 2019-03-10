573ae88f19b3744af8f7875cbfb4b2858674d327
Gregor Petrin <gregap@gmail.com>
2019-03-10 13:56:51 +0100

    Cancel current evalution when the connection closes

--- repl/src/main/java/co/petrin/ScriptHandler.java
+++ repl/src/main/java/co/petrin/ScriptHandler.java
@@ -1,5 +1,6 @@
 package co.petrin;
 
+import co.petrin.function.TriFunction;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
@@ -56,12 +57,12 @@ public class ScriptHandler {
     public Router getRouter(Vertx vertx) {
         var router = Router.router(vertx);
         router.get().handler(this::listDatabases);
-        registerNoDatabasePostHandler(router, "eval", (req) -> createEvaluator().evaluate(null, req));
-        registerNoDatabasePostHandler(router, "suggest", (req) -> createEvaluator().suggest(null, req));
-        registerNoDatabasePostHandler(router, "javadoc", (req) -> createEvaluator().javadoc(null, req));
-        registerDatabasePostHandler(router, "eval", (db, req) -> createEvaluator().evaluate(db, req));
-        registerDatabasePostHandler(router, "suggest", (db, req) -> createEvaluator().suggest(db, req));
-        registerDatabasePostHandler(router, "javadoc", (db, req) -> createEvaluator().javadoc(db, req));
+        registerNoDatabasePostHandler(router, "eval", (evaluator, req) -> evaluator.evaluate(null, req));
+        registerNoDatabasePostHandler(router, "suggest", (evaluator, req) -> evaluator.suggest(null, req));
+        registerNoDatabasePostHandler(router, "javadoc", (evaluator, req) -> evaluator.javadoc(null, req));
+        registerDatabasePostHandler(router, "eval", (evaluator, db, req) -> evaluator.evaluate(db, req));
+        registerDatabasePostHandler(router, "suggest", (evaluator, db, req) -> evaluator.suggest(db, req));
+        registerDatabasePostHandler(router, "javadoc", (evaluator, db, req) -> evaluator.javadoc(db, req));
         return router;
     }
 
@@ -84,7 +85,7 @@ public class ScriptHandler {
      * @param router The router instance on which to register the handlers
      * @param operation The operation-specific path segment that follows the database number.
      */
-    private void registerDatabasePostHandler(Router router, String operation, BiFunction<Database, EvaluationRequest, Object> handler) {
+    private void registerDatabasePostHandler(Router router, String operation, TriFunction<Evaluator, Database, EvaluationRequest, Object> handler) {
         router.postWithRegex("/([0-9]{1,3})/" + operation).handler(BodyHandler.create().setBodyLimit(BODY_SIZE_LIMIT)).blockingHandler( rc -> {
             try {
                 var dbId = Integer.parseInt(rc.request().getParam("param0"));
@@ -92,7 +93,9 @@ public class ScriptHandler {
                         .filter(dbConfig -> dbConfig.id == dbId).findAny()
                         .orElseThrow(() -> new IllegalArgumentException("Database with id " + dbId + " could not be found!"));
                 var request = Json.decodeValue(rc.getBody(), EvaluationRequest.class);
-                var response = handler.apply(db, request);
+                var evaluator = createEvaluator();
+                rc.response().closeHandler(h -> evaluator.stop() );
+                var response = handler.apply(evaluator, db, request);
                 int returnStatus = 200;
                 if (response instanceof EvaluationResponse && ((EvaluationResponse) response).isError()) {
                     returnStatus = 400;
@@ -111,11 +114,13 @@ public class ScriptHandler {
      * @param router The router instance on which to register the handlers
      * @param operation The path on which this handler should be registered
      */
-    private void registerNoDatabasePostHandler(Router router, String operation, Function<EvaluationRequest, Object> handler) {
+    private void registerNoDatabasePostHandler(Router router, String operation, BiFunction<Evaluator, EvaluationRequest, Object> handler) {
         router.postWithRegex("/" + operation).handler(BodyHandler.create().setBodyLimit(BODY_SIZE_LIMIT)).blockingHandler( rc -> {
             try {
                 var request = Json.decodeValue(rc.getBody(), EvaluationRequest.class);
-                var response = handler.apply(request);
+                var evaluator = createEvaluator();
+                rc.response().closeHandler(h -> evaluator.stop());
+                var response = handler.apply(evaluator, request);
                 rc.response().putHeader("content-type", "application/json; charset=UTF-8").end(Json.encodePrettily(response));
             } catch (Throwable t) {
                 rc.response().setStatusCode(500).end("Evaluation caused an unexpected error of type " + t.getClass().getName());
